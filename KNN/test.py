51c5f12d1ef3dc652ee36bad2e165f5d373012fc
zlyang <565213263@qq.com>
2021-09-16 15:39:23 +0800

    test to show the read img

--- KNN/test.py
+++ KNN/test.py
@@ -1,13 +1,34 @@
-from tools import kVector
+# from tools import kVector
 
-v = kVector(3, lambda x:x[0])
+# v = kVector(3, lambda x:x[0])
 
-print(v.data)
-v.push((2,3))
-v.push((1,5))
-v.push((2,6))
-v.push((4,2))
+# print(v.data)
+# v.push((2,3))
+# v.push((1,5))
+# v.push((2,6))
+# v.push((4,2))
 
-print(v.data)
-for i in range(1,10):
-    print(i)
+# print(v.data)
+# for i in range(1,10):
+#     print(i)
+
+import matplotlib.pyplot as plt
+
+with open('data/train-images.idx3-ubyte', 'rb') as file:
+    file.read(4)
+    size = int(file.read(4).hex(), 16)
+    print('size: ', size)
+    rows = int(file.read(4).hex(), 16)
+    columns = int(file.read(4).hex(), 16)
+
+    while True:
+        img = []
+        for i in range(28):
+            row = []
+            for j in range(28):
+                row.append(int(file.read(1).hex(), 16))
+            img.append(row)
+        print('img show')
+        plt.imshow(img, cmap='gray')
+        plt.show()
+        
